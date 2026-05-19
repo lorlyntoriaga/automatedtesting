ace7d8dbaeaa4fceb45e9ea1fe0e06d3b632d8ca
Lorlyn Toriaga <lorlyn@reach52.com>
2026-05-19 16:41:35 +0800

    add reporting for purchase

---
+++ Selenium/qa_automation/reporting.js
@@ -0,0 +1,156 @@
+const {
+  By,
+  Builder,
+  Browser,
+  until,
+  Key,
+  Button,
+} = require("selenium-webdriver");
+const {
+  elementLocated,
+  elementIsVisible,
+} = require("selenium-webdriver/lib/until");
+
+const main = async () => {
+  const driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+  try {
+    await driver.get("https://odoo.uat.reach52.com/web/database/selector");
+
+    // Maximize browser window
+    driver.manage().window().maximize();
+    await driver.sleep(2000);
+    await driver.executeScript("window.scrollBy(0, 500);");
+    await driver.sleep(3000);
+
+    // Shows page title and database
+    const dblistpage = await driver.getTitle();
+    console.log("database list page", dblistpage);
+
+    await driver.sleep(4000);
+
+    // Wait until correct page is loaded
+    await driver.wait(async () => {
+      const url = await driver.getCurrentUrl();
+      return url.includes("database") || url.includes("db=");
+    }, 15000);
+
+    // Wait for DB list container
+    await driver.wait(until.elementLocated(By.css(".list-group")), 15000);
+
+    // Debug: confirm links exist
+    let links = await driver.findElements(By.css("a"));
+    console.log("Links found:", links.length);
+
+    // Click target DB
+    let dbLink = await driver.findElement(
+      By.xpath("//a[contains(@href,'uat_plan_b_review')]"),
+    );
+
+    await dbLink.click();
+    await driver.sleep(2000);
+
+    //wait for the username field to be present and visible
+    const usernameInput = await driver.wait(
+      until.elementLocated(By.id("login")),
+      15000,
+    );
+
+    await driver.wait(until.elementIsVisible(usernameInput), 5000);
+
+    // clear (if needed) and send keys
+    await usernameInput.clear();
+    await usernameInput.sendKeys("odoo.dev2@reach52.com");
+
+    // optionally verify value
+    const value = await usernameInput.getAttribute("value");
+    console.log("username entered:", value);
+
+    // wait for the username field to be present and visible
+    const passwordInput = await driver.wait(
+      until.elementLocated(By.id("password")),
+      15000,
+    );
+
+    await driver.wait(until.elementIsVisible(passwordInput), 5000);
+
+    // clear (if needed) and send keys
+    await passwordInput.clear();
+    await passwordInput.sendKeys("password-R52");
+
+    // optionally verify value
+    const pwdValue = await passwordInput.getAttribute("value");
+    console.log("Password entered:", pwdValue);
+
+    // wait for submit button
+    const loginBtn = await driver.wait(
+      until.elementLocated(By.css("button.btn.btn-primary")),
+      8000,
+    );
+
+    await driver.wait(until.elementIsVisible(loginBtn), 4000);
+    await loginBtn.click();
+    console.log("Login button clicked");
+
+    await driver.sleep(2000);
+
+    // click Purchase App
+    const purchaseBtn = await driver.wait(
+      until.elementLocated(By.id("result_app_5")),
+      8000,
+    );
+
+    await driver.wait(until.elementIsVisible(purchaseBtn), 4000);
+    await purchaseBtn.click();
+    console.log("Purchase button clicked");
+
+    await driver.sleep(2000);
+
+    // Click Order menu
+    const orderbtn = await driver.wait(
+      until.elementLocated(
+        By.css('button[data-menu-xmlid="purchase.purchase_report_main"]'),
+      ),
+      8000,
+    );
+
+    await driver.wait(until.elementIsVisible(orderbtn), 4000);
+    await orderbtn.click();
+    console.log("Reporting menu is clicked");
+
+    await driver.sleep(2000);
+
+    // Vendor menu
+    const vendor = await driver.wait(
+      until.elementLocated(
+        By.css('a[data-menu-xmlid="purchase.purchase_report"]'),
+      ),
+      8000,
+    );
+
+    await driver.wait(until.elementIsVisible(vendor), 4000);
+
+    //  Click using JavaScript (most reliable in Odoo)
+    await driver.executeScript("arguments[0].click();", vendor);
+    console.log("Purchase is clicked");
+
+    await driver.sleep(6000);
+
+    const removeBtn = await driver.findElement(
+      By.xpath("//button[contains(@class,'o_facet_remove')]"),
+    );
+
+    await removeBtn.click();
+    await driver.sleep(6000);
+
+
+    
+
+  } catch (err) {
+    console.error("test failed:", err);
+  } finally {
+    await driver.quit();
+  }
+};
+
+main();
